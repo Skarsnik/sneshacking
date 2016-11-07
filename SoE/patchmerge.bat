be7eb185975566982f16676da5713c463555b1ba
Sylvain Colinet <scolinet@gmail.com>
2016-11-07 11:19:34 +0100

    Revert "SoE: merge savestate and practice modif"
    This reverts commit 7a276a04cb6d67697e2bcf3b1e89f9633499e89e.

--- SoE/patchmerge.bat
+++
@@ -1,2 +0,0 @@
-copy soeexpanded.smc soe_practice.smc
-"D:\Emulation\uoxkas_v06\uoxkas_v06\xkas.exe" "D:\Project\sneshacking\SoE\soesavemain.asm" "D:\Project\sneshacking\SoE\soe_practice.smc"
--- SoE/soesavemain.asm
+++ SoE/soesavemain.asm
@@ -1,8 +1,8 @@
 hirom
-header
+
 ; --- Macros and stuff ---
 incsrc macros.asm ; Useful macros
-incsrc soepractice.asm
+
 ; --- Savestate code ---
 incsrc save_cfg.asm ; Savestate code configuration
 incsrc save.asm ; Savestate code
