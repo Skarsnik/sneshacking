b8c53b7b0fa343f0ee832ee9640b04c6853ab0af
Sylvain Colinet <scolinet@gmail.com>
2017-05-12 15:32:21 +0200

    scompres: extraction working

--- scompress/scompress.c
+++ scompress/scompress.c
@@ -13,27 +13,32 @@
 typedef struct {
     unsigned int	address;
     unsigned char	bpp;
-    bool 		compression;
+    bool 			compression;
+    unsigned int	max_lenght;
 } s_location;
 
 
 char* rom_file = "/home/skarsnik/Zelda - A Link to the Past.smc";
 unsigned int header_offset = 0x200;
 
-#define NB_POINTER_IN_TABLE 222
+#define NB_POINTER_IN_TABLE 223
 
 unsigned int gfx_table_pointers[NB_POINTER_IN_TABLE];
 unsigned int gfx_table_pc_locations[NB_POINTER_IN_TABLE];
 
-/* Most data in the table are 3bpp, zcompress convert them to 4bpp
-* some data are stored as 2bpp, zcompress extract them as 2bpp
+/* zcompress skip some data. Here are all the data :
+* 00-DC are extracted from the pointer table
+* 00-70  : 3bpp compressed (converted to 4bpp)
+* 71, 72 : 2bpp compressed
+* 73-7E  : 3bpp uncompressed (converted to 4bpp)
+* 7F-D9  : 3bpp compressed (converted to 4bpp)
+* DA-DC  : 2bpp compressed
+* DD,DE  : 2bpp compressed, not present in zcompress
+
 */
 
-#define TABLE_INDEX2BPP_SIZE 6
-
-unsigned char index_2bpp_compressed[TABLE_INDEX2BPP_SIZE] = {0x71, 0x72, 0xDA, 0xDB, 0xDC, 0xDD};
-
-unsigned char index_2bpp_uncompressed[] = {0xDD};
+s_location   gfx_locations[NB_POINTER_IN_TABLE];
+s_location   zcompress_gfx_locations[NB_POINTER_IN_TABLE];
 
 void set_header_offset()
 {
@@ -77,19 +82,6 @@ int	rom_fseek(FILE* stream, long offset)
     return fseek(stream, offset + header_offset, SEEK_SET);
 }
 
-static char*	hexString(const char* str, const unsigned int size)
-{
-    char* toret = malloc(size * 3 + 1);
-
-    unsigned int i;
-    for (i = 0; i < size; i++)
-    {
-        sprintf(toret + i * 3, "%02X ", (unsigned char) str[i]);
-    }
-    toret[size * 3] = 0;
-    return toret;
-}
-
 // ROM_DATA[0x04F80 + 446 + i], ROM_DATA[0x04F80 + 223 + i], ROM_DATA[0x04F80 + i]
 
 void	build_gfx_table_pointers()
@@ -121,6 +113,79 @@ void	build_gfx_table_pointers()
     fclose(rom_stream);
 }
 
+void	build_gfx_location()
+{
+    for (unsigned int i = 0; i < NB_POINTER_IN_TABLE; i++)
+    {
+        gfx_locations[i].address = gfx_table_pc_locations[i];
+        gfx_locations[i].max_lenght = 0;
+        if (i >= 0 && i <= 0x70)
+        {
+            gfx_locations[i].bpp = 3;
+            gfx_locations[i].compression = true;
+            if (i != 0x70)
+                gfx_locations[i].max_lenght = gfx_table_pc_locations[i + 1] - gfx_locations[i].address;
+            else
+                gfx_locations[i].max_lenght = gfx_locations[i - 1].max_lenght;
+        }
+        if (i == 0x71 || i == 0x72)
+        {
+            gfx_locations[i].bpp = 2;
+            gfx_locations[i].compression = true;
+            if (i == 0x71)
+                gfx_locations[i].max_lenght = gfx_table_pc_locations[i + 1] - gfx_locations[i].address;
+            else
+                gfx_locations[i].max_lenght = 2048;
+        }
+        if (i >= 0x73 && i <= 0x7E)
+        {
+            gfx_locations[i].bpp = 3;
+            gfx_locations[i].compression = false;
+            if (i != 0x7E)
+                gfx_locations[i].max_lenght = gfx_table_pc_locations[i + 1] - gfx_locations[i].address;
+            else
+                gfx_locations[i].max_lenght = gfx_locations[i - 1].max_lenght;
+        }
+        if (i >= 0x7F && i <= 0xD9)
+        {
+            gfx_locations[i].bpp = 3;
+            gfx_locations[i].compression = true;
+            if (i != 0xD9)
+                gfx_locations[i].max_lenght = gfx_table_pc_locations[i + 1] - gfx_locations[i].address;
+            else
+                gfx_locations[i].max_lenght = gfx_locations[i - 1].max_lenght;
+
+        }
+        if (i >= 0xDA && i <= 0xDE)
+        {
+            gfx_locations[i].bpp = 2;
+            gfx_locations[i].compression = true;
+            if (i != 0xDE)
+                gfx_locations[i].max_lenght = gfx_table_pc_locations[i + 1] - gfx_locations[i].address;
+            else
+                gfx_locations[i].max_lenght = gfx_locations[i - 1].max_lenght;
+        }
+    }
+}
+
+void	build_zcompress_gfx_location()
+{
+    for (unsigned int i = 0; i < NB_POINTER_IN_TABLE; i++)
+    {
+        zcompress_gfx_locations[i].bpp = gfx_locations[i].bpp;
+        zcompress_gfx_locations[i].compression = gfx_locations[i].compression;
+        zcompress_gfx_locations[i].address = gfx_locations[i].address;
+		zcompress_gfx_locations[i].max_lenght = gfx_locations[i].max_lenght;
+    }
+    zcompress_gfx_locations[0xDD].address = 0x70000;
+    zcompress_gfx_locations[0xDD].bpp = 2;
+    zcompress_gfx_locations[0xDD].compression = false;
+    zcompress_gfx_locations[0xDD].max_lenght = 2048 * 2;
+    zcompress_gfx_locations[0xDE].address = 0x80000;
+    zcompress_gfx_locations[0xDE].bpp = 4;
+    zcompress_gfx_locations[0xDE].compression = false;
+    zcompress_gfx_locations[0xDE].max_lenght = 28672;
+}
 
 // Just list the table
 void	list_table_compressed()
@@ -151,91 +216,140 @@ char*	convert_3bpp_to_4bpp(char *tiles, unsigned int lenght, unsigned int* conve
     return converted;
 }
 
-bool	is_index_2bpp_compressed(unsigned int index)
+void	print_location(s_location loc)
 {
-    for (unsigned int i = 0; i < TABLE_INDEX2BPP_SIZE; i++)
-    {
-        if (index_2bpp_compressed[i] == index)
-            return true;
-    }
-    return false;
+  printf("0x%06X - %d bpp,  %s  - max lenght: %d\n", loc.address, loc.bpp,
+               loc.compression ? "Compressed" : "Uncompressed", loc.max_lenght);
 }
 
-bool	is_index_2bpp_uncompressed(unsigned int index)
+
+void	print_gfx_location()
 {
-    for (unsigned int i = 0; i < sizeof(index_2bpp_uncompressed); i++)
+    for (unsigned int i = 0; i < NB_POINTER_IN_TABLE; i++)
     {
-        if (index_2bpp_uncompressed[i] == index)
-            return true;
+        printf("%02X  ", i);
+		print_location(gfx_locations[i]);
     }
-    return false;
 }
 
-// Extract all gfx in separate file
-void	extract_gfx_separated()
+
+void	extract_gfx(s_location* locations, bool single_file, const char* allfilename)
 {
-    set_header_offset();
-    build_gfx_table_pointers();
+    char read_buffer[1000000]; //~ 1M should be enought x)
     char filename[20];
-    char buffer[2048];
     FILE* rom_stream = my_fopen(rom_file, "r");
+	FILE* gfx_file;
     unsigned int	compressed_lenght;
     unsigned int	decompressed_lenght;
+    unsigned int	to_convert_lenght;
     unsigned int	converted_lenght;
-    bool		is_compressed = false;
+    char			*data;
+    unsigned int	read_lenght;
+    char			*uncompressed;
+    char			*to_write;
+    unsigned int	write_lenght;
+    s_location		location;
+	
+	if (single_file)
+	  gfx_file = my_fopen(allfilename, "w");
 
     for (unsigned int i = 0; i < NB_POINTER_IN_TABLE; i++)
     {
-        sprintf(filename, "SGFX%02X.bin", i);
-        unsigned int pc_location = gfx_table_pc_locations[i];
-        printf("Creating %s from SNES:%06X | PC:%06X\n", filename, gfx_table_pointers[i], gfx_table_pc_locations[i]);
-        FILE* gfx_file = my_fopen(filename, "w");
+        location = locations[i];
+        unsigned int pc_location = location.address;
+
+		printf("==%02X==\n", i);
+		print_location(location);
         rom_fseek(rom_stream, pc_location);
-        fread(buffer, 2048, 1, rom_stream);
-        is_compressed = (! (pc_location >= 0x87000 && pc_location < 0x8B7FF) );// && !is_index_2bpp_uncompressed(i);
-        if (is_compressed)
+        read_lenght = fread(read_buffer, 1, location.max_lenght, rom_stream);
+        write_lenght = read_lenght;
+        to_convert_lenght = (2048 / 32) * 24;
+        data = read_buffer;
+        if (location.compression)
         {
-            char* uncompressed = alttp_decompress(buffer, 0, 2048, &decompressed_lenght, &compressed_lenght);
+            printf("Compressed\n");
+            uncompressed = alttp_decompress(read_buffer, 0, read_lenght, &decompressed_lenght, &compressed_lenght);
+			printf("Compressed lenght : %d  - Decompressed lenght : %d\n", compressed_lenght, decompressed_lenght);
+			if (read_lenght < compressed_lenght)
+			  fprintf(stderr, "WARNING: compressed lenght read exceed the expected read lenght for : %02X\n", i);
             if (uncompressed == NULL)
             {
                 fprintf(stderr, "Error while decompressing data for 0x%06X\n", pc_location);
                 exit(1);
             }
-            if (is_index_2bpp_compressed(i))
+            data = uncompressed;
+            to_convert_lenght = decompressed_lenght;
+        }
+        if (location.bpp == 3)
+        {
+            char *converted = convert_3bpp_to_4bpp(data, to_convert_lenght, &converted_lenght);
+            write_lenght = converted_lenght;
+            to_write = converted;
+        }
+        if (location.bpp == 2)
+        {
+            if (location.compression)
             {
-	        printf("Compressed, 2bpp, no convertion.\n");
-                fwrite(uncompressed, decompressed_lenght, 1, gfx_file);
-                fseek(gfx_file, 2048, SEEK_SET);
+                to_write = (char*) malloc(2048);
+                memcpy(to_write, data, decompressed_lenght);
+				if (decompressed_lenght != 2048)
+                  memset(to_write, 0, decompressed_lenght - 1);
+                write_lenght = 2048;
             } else {
-	        printf("Compressed, standard, converstion 3bpp to 4bpp.\n");
-                char *converted = convert_3bpp_to_4bpp(uncompressed, decompressed_lenght, &converted_lenght);
-                fwrite(converted, converted_lenght, 1, gfx_file);
-                free(converted);
-            }
-            free(uncompressed);
-        } else {
-            if (is_index_2bpp_uncompressed(i))
-            {
-	        printf("No compression, 2bpp, no convertion\n");
-		fwrite(buffer, 2048, 1, gfx_file);
-            } else {
-	        printf("No compression, conversion 3bpp to 4bpp\n");
-                char *converted = convert_3bpp_to_4bpp(buffer, (2048 / 32) * 24, &converted_lenght);
-                fwrite(converted, converted_lenght, 1, gfx_file);
-                free(converted);
+                write_lenght = location.max_lenght;
+                to_write = (char*) malloc(write_lenght);
+                memcpy(to_write, read_buffer, write_lenght);
             }
         }
-        fclose(gfx_file);
+        if (location.bpp == 4)
+        {
+            write_lenght = location.max_lenght;
+            to_write = (char*) malloc(write_lenght);
+            memcpy(to_write, read_buffer, write_lenght);
+        }
+        if (location.compression)
+            free(uncompressed);
+
+        if (single_file)
+        {
+			fwrite(to_write, 1, write_lenght, gfx_file);
+        } else {
+            sprintf(filename, "SGFX%02X.bin", i);
+            gfx_file = my_fopen(filename, "w");
+            fwrite(to_write, 1, write_lenght, gfx_file);
+            fclose(gfx_file);
+        }
+        free(to_write);
     }
-    fclose(rom_stream);
+    if (single_file)
+	  fclose(gfx_file);
+	fclose(rom_stream);
 }
+
 
 int 	main(int ac, char *ag[])
 {
 
     if (ac == 3 && ag[1][0] == '0')
-        extract_gfx_separated();
+    {
+        set_header_offset();
+        build_gfx_table_pointers();
+        build_gfx_location();
+        build_zcompress_gfx_location();
+        extract_gfx(zcompress_gfx_locations, true, "alttpall.bin");
+    }
     if (ac ==  3 && ag[1][0] == '2')
+    {
+        set_header_offset();
         list_table_compressed();
+    }
+    if (ac == 3 && ag[1][0] == '3')
+    {
+        set_header_offset();
+        build_gfx_table_pointers();
+        build_gfx_location();
+        print_gfx_location();
+    }
     return 0;
 }
+
